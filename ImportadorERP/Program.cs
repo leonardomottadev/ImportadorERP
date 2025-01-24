817779dafb17573dd17c24e3dd61d4fa823fd416
Leonardo Motta de Oliveira <leonardomottadev@gmail.com>
2025-01-24 13:53:57 -0300

    start

---
+++ ImportadorERP/Program.cs
@@ -0,0 +1,18 @@
+﻿using ImportadorERP;
+
+Console.WriteLine("Hello, World!");
+
+// obter o Layout
+Record[] layouts = LayoutManager.GetCompleteClientLayout();
+
+List<int> index_list = layouts.Select(layout => layout.Index).ToList();
+
+// abrir arquivos na pasta selecionada para a importação
+
+// realizar a leitura dos dados com base no layout 
+
+// Converter para o layout
+
+// Salvar em um novo arquivo .txt
+
+Console.ReadKey();
---
+++ ImportadorERP/Record.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportadorERP
+{
+    public class Record
+    {
+        private int index;
+        private int size;
+        private string format;
+        private string data;
+
+        public Record(int index, int size, string format)
+        {
+            this.index = index;
+            this.size = size;
+            this.format = format;
+            this.data = "";
+        }
+
+        public string Data { get => data; set => data = value; }
+        public string Format { get => format; set => format = value; }
+        public int Size { get => size; set => size = value; }
+        public int Index { get => index; set => index = value; }
+    }
+}
